95c8edbbe14e7bbdd4a2f4cd6c339d10c8ea1f7f
drethereum <trevor.wenokur@gmail.com>
2024-02-13 09:28:21 -0700

    create udfs edit

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,27 +1,19 @@
 {% macro create_udfs() %}
     {% if var("UPDATE_UDFS_AND_SPS") %}
-
         {% set sql %}
         CREATE schema if NOT EXISTS silver;
             {{ create_udtf_get_base_table(
             schema = "streamline"
         ) }}
+        {{ create_udf_get_chainhead() }}
+        {{ create_udf_json_rpc() }}
+        {{ create_udf_rest_api() }}
+        {{ create_udf_api() }}
+        {{ create_udf_decode_array_string() }}
+        {{ create_udf_decode_array_object() }}
+        {{ create_udf_bulk_decode_logs() }}
         {% endset %}
         {% do run_query(sql) %}
-
-            {% if target.database != "BLAST_COMMUNITY_DEV" %}
-                {% set sql %}
-                {{ create_udf_get_chainhead() }}
-                {{ create_udf_json_rpc() }}
-                {{ create_udf_rest_api() }}
-                {{ create_udf_api() }}
-                {{ create_udf_decode_array_string() }}
-                {{ create_udf_decode_array_object() }}
-                {{ create_udf_bulk_decode_logs() }}
-                {% endset %}
-                {% do run_query(sql) %}
-            {% endif %}
-
-        {{ - fsc_utils.create_udfs() - }}
+        {{- fsc_utils.create_udfs() -}}
     {% endif %}
 {% endmacro %}
