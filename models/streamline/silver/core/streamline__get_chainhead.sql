40924a1c616c15bdbd4c5b194174b1bc8b795ddf
drethereum <trevor.wenokur@gmail.com>
2024-02-13 19:14:28 -0700

    tbl config

--- models/streamline/silver/core/streamline__get_chainhead.sql
+++ models/streamline/silver/core/streamline__get_chainhead.sql
@@ -1,41 +1,19 @@
 {{ config (
-    materialized = 'incremental',
-    incremental_strategy = 'delete+insert',
-    unique_key = ['block_number'],
+    materialized = 'table',
     tags = ['streamline_core_complete']
 ) }}
 
-WITH get_chainhead AS (
-
-    SELECT
-        live.udf_api(
-            'POST',
-            '{blast_testnet_url}',
-            --update for prod
-            {},{ 'method' :'eth_blockNumber',
-            'params' :[],
-            'id' :1,
-            'jsonrpc' :'2.0' },
-            'quicknode_blast_testnet' --update for prod
-        ) AS resp,
-        utils.udf_hex_to_int(
-            resp :data :result :: STRING
-        ) AS block_number,
-        SYSDATE() AS _inserted_timestamp
-
-{% if is_incremental() %}
-WHERE
-    block_number NOT IN (
-        SELECT
-            DISTINCT block_number
-        FROM
-            {{ this }}
-    )
-{% endif %}
-)
 SELECT
-    block_number
-FROM
-    get_chainhead
-ORDER BY block_number DESC
-LIMIT 1
+    live.udf_api(
+        'POST',
+        '{blast_testnet_url}',
+        --update for prod
+        {},{ 'method' :'eth_blockNumber',
+        'params' :[],
+        'id' :1,
+        'jsonrpc' :'2.0' },
+        'quicknode_blast_testnet' --update for prod
+    ) AS resp,
+    utils.udf_hex_to_int(
+        resp :data :result :: STRING
+    ) AS block_number
