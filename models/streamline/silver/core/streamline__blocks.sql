51b2b26a6da4d90b2aeda9a223a23c496f1afb1c
drethereum <trevor.wenokur@gmail.com>
2024-02-13 18:56:20 -0700

    chainhead table

--- models/streamline/silver/core/streamline__blocks.sql
+++ models/streamline/silver/core/streamline__blocks.sql
@@ -3,23 +3,6 @@
     tags = ['streamline_core_complete']
 ) }}
 
-WITH get_chainhead AS (
-
-    SELECT
-        live.udf_api(
-            'POST',
-            '{blast_testnet_url}',
-            --update for prod
-            {},{ 'method' :'eth_blockNumber',
-            'params' :[],
-            'id' :1,
-            'jsonrpc' :'2.0' },
-            'quicknode_blast_testnet' --update for prod
-        ) AS resp,
-        utils.udf_hex_to_int(
-            resp :data :result :: STRING
-        ) AS block_number
-)
 SELECT
     _id AS block_number,
     REPLACE(
@@ -37,7 +20,7 @@ WHERE
                 0
             )
         FROM
-            get_chainhead
+            {{ref("streamline__get_chainhead")}}
     )
 ORDER BY
     _id ASC
