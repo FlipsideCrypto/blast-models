86e4d46db49343fd8592607872fdd77b890edefb
drethereum <trevor.wenokur@gmail.com>
2024-02-22 12:52:39 -0700

    prod

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -4,7 +4,7 @@
         json OBJECT
     ) returns ARRAY api_integration = 
     {% if target.name == "prod" %}
-        aws_blast_api AS 'insert-prod-url-here'
+        aws_blast_api AS 'https://42gzudc5si.execute-api.us-east-1.amazonaws.com/prod/udf_bulk_rest_api'
     {% else %}
         aws_blast_api_dev AS 'https://05340o05al.execute-api.us-east-1.amazonaws.com/dev/udf_bulk_rest_api'
     {%- endif %};
