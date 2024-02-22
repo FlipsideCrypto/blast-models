251520f55020690f5fba3a900303cc5748039ae9
drethereum <71602799+drethereum@users.noreply.github.com>
2024-02-22 12:55:02 -0700

    prod (#5)

--- macros/streamline/api_integrations.sql
+++ macros/streamline/api_integrations.sql
@@ -2,16 +2,14 @@
     {{ log(
         "Creating integration for target:" ~ target
     ) }}
-    {# {% if target.name == "prod" %}
+    {% if target.name == "prod" %}
     {% set sql %}
-    CREATE api integration IF NOT EXISTS aws_blast_api api_provider = aws_api_gateway api_aws_role_arn = 'insert-prod-arn-here' api_allowed_prefixes = (
-        'insert-prod-url-here'
+    CREATE api integration IF NOT EXISTS aws_blast_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::924682671219:role/blast-api-prod-rolesnowflakeudfsAF733095-DY5ob2RfHqOI' api_allowed_prefixes = (
+        'https://42gzudc5si.execute-api.us-east-1.amazonaws.com/prod/'
     ) enabled = TRUE;
 {% endset %}
     {% do run_query(sql) %}
-    #}
-    {% if target.name == "dev" %}
-        --replace if with elif after prod is deployed
+    {% elif target.name == "dev" %}
         {% set sql %}
         CREATE api integration IF NOT EXISTS aws_blast_api_dev api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::704693948482:role/blast-api-dev-rolesnowflakeudfsAF733095-Wtkj0DGJ7lOQ' api_allowed_prefixes = (
             'https://05340o05al.execute-api.us-east-1.amazonaws.com/dev/'
