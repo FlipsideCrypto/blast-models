d1e8e29c45504d2a2243c10531e1180420c710bb
drethereum <trevor.wenokur@gmail.com>
2024-02-26 11:11:48 -0700

    secret

--- models/streamline/silver/core/history/streamline__traces_history.sql
+++ models/streamline/silver/core/history/streamline__traces_history.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_rest_api(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'traces', 'exploded_key','[\"result\"]', 'sql_limit', {{var('sql_limit','100000')}}, 'producer_batch_size', {{var('producer_batch_size','100000')}}, 'worker_batch_size', {{var('worker_batch_size','50000')}}, 'sm_secret_name','prod/blast/node/mainnet'))",
+        func = "{{this.schema}}.udf_rest_api(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'traces', 'exploded_key','[\"result\"]', 'sql_limit', {{var('sql_limit','100000')}}, 'producer_batch_size', {{var('producer_batch_size','100000')}}, 'worker_batch_size', {{var('worker_batch_size','50000')}}, 'sm_secret_name','prod/blast/mainnet'))",
         target = "{{this.schema}}.{{this.identifier}}"
     ),
     tags = ['streamline_core_history']
