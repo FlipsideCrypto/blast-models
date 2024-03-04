3846e0f362839c056a2a904c0c4a19269e01ac96
drethereum <trevor.wenokur@gmail.com>
2024-03-04 15:41:09 -0700

    coalesce

--- models/gold/core/core__fact_token_transfers.sql
+++ models/gold/core/core__fact_token_transfers.sql
@@ -17,19 +17,8 @@ SELECT
     to_address,
     raw_amount,
     raw_amount_precise,
-    COALESCE (
-        transfers_id,
-        {{ dbt_utils.generate_surrogate_key(
-            ['tx_hash', 'event_index']
-        ) }}
-    ) AS fact_token_transfers_id,
-    COALESCE(
-        inserted_timestamp,
-        '2000-01-01'
-    ) AS inserted_timestamp,
-    COALESCE(
-        modified_timestamp,
-        '2000-01-01'
-    ) AS modified_timestamp
+    transfers_id AS fact_token_transfers_id,
+    inserted_timestamp,
+    modified_timestamp
 FROM
     {{ ref('silver__transfers') }}
