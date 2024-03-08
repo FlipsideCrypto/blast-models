9805604b2e29cc2215cee2923f1ea0acd2c41268
drethereum <trevor.wenokur@gmail.com>
2024-03-08 10:33:04 -0700

    decoded logs streamline, silver, core and macros

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -9,3 +9,14 @@
         aws_blast_api_dev AS 'https://y9d0tuavh6.execute-api.us-east-1.amazonaws.com/stg/udf_bulk_rest_api'
     {%- endif %};
 {% endmacro %}
+
+{% macro create_udf_bulk_decode_logs() %}
+    CREATE
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_decode_logs(
+        json OBJECT
+    ) returns ARRAY api_integration = {% if target.name == "prod" %}
+        aws_blast_api AS 'https://42gzudc5si.execute-api.us-east-1.amazonaws.com/prod/bulk_decode_logs'
+    {% else %}
+        aws_blast_api_dev AS'https://y9d0tuavh6.execute-api.us-east-1.amazonaws.com/stg/bulk_decode_logs'
+    {%- endif %};
+{% endmacro %}
