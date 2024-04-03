13c62d60ef692c1fbf67f2276bbb6a8c6cf8e40d
mattromano <mattromano88@gmail.com>
2024-04-03 10:46:10 -0700

    add blitz

---
+++ macros/tests/blitz_missing_products.sql
@@ -0,0 +1,45 @@
+{% test blitz_missing_products(
+    model,
+    filter) %}
+
+with recent_records as (
+    select * from  {{model}}
+    where modified_timestamp >= SYSDATE() - INTERVAL '12 hours'
+),
+
+invalid_product_ids as (
+    select distinct product_id
+    from {{ ref('silver__blitz_dim_products') }}
+    where product_id not in (select product_id from recent_records)
+    {% if filter %}
+        AND {{ filter }}
+    {% endif %}
+)
+
+select * 
+from invalid_product_ids
+
+{% endtest %}
+
+{% test blitz_product_level_recency(
+    model,
+    filter) %}
+
+with recent_records as (
+    select distinct(product_id) from  {{model}}
+    where block_timestamp >= SYSDATE() - INTERVAL '1 DAY'
+),
+
+invalid_product_ids as (
+    select *
+    from {{ ref('silver__blitz_dim_products') }}
+    where product_id not in (select product_id from recent_records)
+    {% if filter %}
+        AND {{ filter }}
+    {% endif %}
+)
+
+select * 
+from invalid_product_ids
+
+{% endtest %}
